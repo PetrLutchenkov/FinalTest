ee3c4e2aa5f9ec1210d39c3a5df41a10c6e727bc
Petr <gram07@list.ru>
2023-01-24 14:09:19 +0300

    FinalTest completed

--- FinalTest/Program.cs
+++ FinalTest/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] firstArray = new string[] { "hello", "world", "1234", "-2", ";-)", "Russia" };
+string[] secondArray = new string[firstArray.Length];
+SecondArrayAdd(firstArray, secondArray);
+Print1DArr(firstArray);
+Print1DArr(secondArray);
+
+void SecondArrayAdd(string[] array, string[] newArray)
+{
+    int n = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+
+        {
+            newArray[n] = array[i];
+            n++;
+        }
+    }
+}
+void Print1DArr(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        if (arr[i] != null)
+            Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
